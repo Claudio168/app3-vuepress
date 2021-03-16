379fa3153df5f9678bb433a1003cc3555e136ddc
Claudio168 <claudiogonzalez0712@gmail.com>
2021-03-15 23:12:30 -0300

    tercer

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'contact', link: '/contact/' },
+        { text: 'about', link: '/about/' },
+        { text: 'External', link: 'https://google.com' }
+      ]
+      
+  
+    }
+  }
