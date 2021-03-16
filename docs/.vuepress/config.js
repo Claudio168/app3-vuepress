55b6b1c511f53cd0d37360c76efe392e04d443c0
Claudio168 <claudiogonzalez0712@gmail.com>
2021-03-15 23:25:03 -0300

    dddd

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    base: '/.vuepress/dist/docs',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'about', link: '/about/' },
+        { text: 'contact', link: '/contact/' },
+        { text: 'External', link: 'https://google.com' }
+      ]
+      
+    }
+  }
